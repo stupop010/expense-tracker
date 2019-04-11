f09050efd3b7d704277cefcecc4cdc5ec9d0d7a4
Stuart <stupop010@hotmail.co.uk>
2019-04-11 14:11:39 +0100

    changed loader

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import "./loader.css";
 
-const Loader = ({ sentence }) => {
+const Loader = () => {
   return (
     <div className="loader">
       <div className="box1">1</div>
       <div className="box2">2</div>
       <div className="box3">3</div>
       <div className="box4">4</div>
-      <h2>Loading: {sentence}</h2>
+      <h2>Loading...</h2>
     </div>
   );
 };
--- client/src/components/recentLog/RecentLog.js
+++ client/src/components/recentLog/RecentLog.js
@@ -40,7 +40,7 @@ class RecentLog extends Component {
     return (
       <div className="card-contaniner">
         {loading ? (
-          <Loader sentence={"Expenses"} />
+          <Loader />
         ) : (
           <Log expense={expense} error={this.state.msg} />
         )}
