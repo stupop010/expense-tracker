b50e462ef4cd5015bf52b6ba65c16ad1922b00d1
Stuart <stupop010@hotmail.co.uk>
2019-04-21 15:45:38 +0100

    production mode

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ app.use("/expense", require("./routes/expenseRoutes.js"));
 app.use("/auth", require("./routes/googleAuth.js"));
 app.use("/register", require("./routes/registerRoutes.js"));
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 mongoose
   .connect(keys.mongoDB, { useNewUrlParser: true })
   .then(() => {
